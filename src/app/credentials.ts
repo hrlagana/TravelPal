24b87ddcc7c9482bd5b17a0a66bbb1fc501a3bc7
hrlagana <hrlagana@gmail.com>
2018-05-13 19:02:31 +0300

    Update credentials.ts

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,9 +1,9 @@
 // Initialize Firebase
 export const firebaseConfig = {
-    apiKey: "AIzaSyAMXFGqwJQP_ffWx12uPGWQDNjXGDGKiro",
-    authDomain: "eventmanager-b4218.firebaseapp.com",
-    databaseURL: "https://eventmanager-b4218.firebaseio.com",
-    projectId: "eventmanager-b4218",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "608818035720"
+    messagingSenderId: ""
 };
